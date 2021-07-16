f8bda33b292ffe23dd0eefd965021c09e112cf47
Nezox-art <79809240+Nezox-art@users.noreply.github.com>
2021-07-17 00:00:01 +0300

    Update update_flights.php

--- update_flights.php
+++ update_flights.php
@@ -12,70 +12,41 @@
 	}
 }
 
-if (isset($_GET['red_id'])) {
-	$sql_select = "SELECT ID_Flights,Destination,Departure_Time,Type FROM flights, aircraft WHERE ID_Plane=ID_Aircraft AND ID_Flights = {$_GET['red_id']}";
-	$result_select = mysqli_query($link, $sql_select);
-	$row = mysqli_fetch_array($result_select);
-}
+
+if(isset($_GET['redakt']))
+{
+    $id = mysql_real_escape_string($_GET['redakt'], $link);
+
+
+    $query ="SELECT Destination,Departure_Time,Type FROM flights, aircraft WHERE ID_Plane=ID_Aircraft AND ID_Flights = '".$id."'";
+
+    $result = mysql_query($query, $link) or die("Ошибка " . mysql_error($link));
+
+    if($result && mysql_num_rows($result)>0)
+    {
+        $row = mysql_fetch_row($result);
+        $Destination = $row[0];
+        $Departure_Time = $row[1];
+        $Type = $row[2];
+
+        echo "<center><h2>Изменить информацию о рейсе</h2>
+            <form method='POST'>
+            <input type='hidden' name='ID_Flights' value='$ID_Flights' />
+            <p>Место назначения<br>
+            <input type='text' name='Destination' maxlength='50' value='$Destination' /></p>
+						<p>Время отправления<br>
+            <input type='time' name='Departure_Time' maxlength='50' value='$Departure_Time' /></p>
+            <p>Должность:
+            <select name='Type'>";
+                        $query1='SELECT Type, ID_Aircraft FROM aircraft';
+                        $ID_Aircraft = $row['ID_Aircraft'];
+                        $res = mysql_query($query1);
+                        while($row = mysql_fetch_assoc($res)){
+        echo '<option value="'.$row['ID_Aircraft'].'"'.($row['ID_Aircraft']==$ID_Aircraft?' selected':'').'>'.$row['Type'].'</option>;';
+                        }
+
+            echo "</select><br>
+
+            <input type='submit' name='submit3' value='Сохранить'>
+            </form></center>";
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-	<form action="" method="POST">
-		<table>
-			<tr>
-			<td>Место назначения</td>
-			<td><input type="text" name="Destination" value="<?=isset($_GET['red_id']) ? $row['Destination'] : ''; ?>"></td>
-			</tr>
-			<tr>
-			<td>Время отправления</td>
-			<td><input type="time" name="Departure_Time" value="<?=isset($_GET['red_id']) ? $row['Departure_Time'] : ''; ?>"></td>
-			</tr>
-			<tr>
-				<td>Выберете тип самолета</td>
-				<td><select name='ID_Plane'>";
-					<?php
-				    include 'connect.php';
-						$query1='SELECT ID_Aircraft ,Type FROM aircraft';
-				    $id_dolgnosti = $row['ID_Aircraft'];
-				    $res = mysql_query($query1);
-						while($row = mysql_fetch_assoc($res)){
-							echo '<option value="'.$row['ID_Aircraft'].'"'.($row['ID_Aircraft']==$id_dolgnosti?' selected':'').'>'.$row['Type'].'</option>;';
-						}
-						?>
-					</select>
-					</td>
-
-				<td><select name="ID_Plane">
-					<?php
-					include 'connect.php';
-					$sql_select = "SELECT ID_Aircraft,Type FROM aircraft";
-					$result_select = mysqli_query($link, $sql_select);
-
-					while ($row = mysqli_fetch_array($result_select))
-					{
-							echo "<option value = '".$row['ID_Plane']."'>".$row['Type']."</option>";
-					}
-					?>
-				</select>
-			</td>
-
-
-
-			</tr>
-			<tr>
-				<td colspan="2">
-					<input type="submit" name="Сохранить">
-				</td>
-			</tr>
-		</table>
-	</form>
-	<form action="index_flights_admin.php" method="POST">
-		<input type="submit" value="Вернуться назад">
-	</form>
-</body>
-</html>
